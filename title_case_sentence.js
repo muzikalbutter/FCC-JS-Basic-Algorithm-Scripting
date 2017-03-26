bce8735db89b95c020d7593bf1e76d3f043508dc
Holly Zuckerman <butterbme1128@yahoo.com>
2017-03-26 11:40:53 -0400

    Already completed algorithm

--- title_case_sentence.js
+++ title_case_sentence.js
@@ -1,3 +1,9 @@
+/* Return the provided string with the first letter of each word capitalized. 
+Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words 
+like "the" and "of".*/
+
 function titleCase(str) {
   var allLower = str.toLowerCase();
   var array = allLower.split(" ");
@@ -15,3 +21,10 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+//should return a string.
+titleCase("I'm a little tea pot");
+//should return "I'm A Little Tea Pot".
+titleCase("sHoRt AnD sToUt");
+//should return "Short And Stout".
+titleCase("HERE IS MY HANDLE HERE IS MY SPOUT");
+//should return "Here Is My Handle Here Is My Spout".
