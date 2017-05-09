3c5aa81fcf9fbb5d664714482b8bd316a57b5555
Holly Zuckerman <butterbme1128@yahoo.com>
2017-05-09 14:53:20 -0400

    Partially working algorithm

---
+++ seek_and_destroy.js
@@ -0,0 +1,23 @@
+/*You will be provided with an initial array (the first argument
+in the destroyer function), followed by one or more arguments.
+Remove all elements from the initial array that are of the same
+value as these arguments.*/
+
+function destroyer(arr) {
+  // Remove all the values
+  var args = [].slice.call(arguments);
+  var arrOnly = args.shift();
+  function keepValue(value){
+    for (var i = 0; i < args.length; i++){
+      return value !== args[i];
+    }
+  }
+  var newArr = arrOnly.filter(keepValue);
+  return newArr;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3); //should return [1, 1].
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3); //should return [1, 5, 1].
+destroyer([3, 5, 1, 2, 2], 2, 3, 5); //should return [1].
+destroyer([2, 3, 2, 3], 2, 3); //should return [].
+destroyer(["tree", "hamburger", 53], "tree", 53); //should return ["hamburger"]
